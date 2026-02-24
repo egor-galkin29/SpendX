d09e7b2331293276ecaf27ca5ed982a1eb4d96d0
egor-galkin29 <egorgalkin290907@gmail.com>
2026-02-24 08:57:07 -0500

    Initial Commit

---
+++ SpendX/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SpendX
+//
+//  Created by Егор Галкин on 2026-02-24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
